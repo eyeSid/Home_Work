1ba91787f5dc6aaea737508ea2d998d17cac9e41
Sidorov_S.V <126830059+eyeSid@users.noreply.github.com>
2023-03-10 14:57:34 +0300

    Update HW42.java

--- lesson4/HW42.java
+++ lesson4/HW42.java
@@ -3,9 +3,14 @@ package lesson4;
 public class HW42 {
 
     public static void main(String[] args) {
+/*  
         System.out.println("положительные степени числа 5:");
         for ( long a = 1 ; Math.pow(5, a) < 10000 ; a = a + 1) {
             System.out.println("5 в степени " + a + " = " + Math.pow(5, a));
+        }
+*/      
+        for (int i = 1; i < 10000; i = i * 5) {
+            System.out.println(i);
         }
     }
 }
