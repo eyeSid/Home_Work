258a0c60208eca86320379585a9b24cf3d153f12
Sid&Du <sidorov.s.v@live.com>
2023-03-12 15:36:24 +0300

    тест гит хаба 2

--- lesson6/less6.java
+++ lesson6/less6.java
@@ -3,5 +3,7 @@ package lesson6;
 public class less6 {
     public static void main(String[] args) {
         System.out.println("тест Гит Хаба");
+        System.out.println("тест Гит Хаба2");
+
     }
 }
