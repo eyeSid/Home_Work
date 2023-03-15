7ee92904d6980ad40c8c18645856584e7cc2cdf3
Sid&Du <sidorov.s.v@live.com>
2023-03-15 03:23:12 +0300

    14.03.2023-2

--- lesson5/less52.java
+++ lesson5/less52.java
@@ -2,6 +2,7 @@ package lesson5;
 
 public class less52 {
     public static void main(String[] args) {
+
         int b = 1000;
         int [] a = new int[b];
         for (int i = 0; i < b; i++) {
@@ -12,5 +13,31 @@ public class less52 {
             sum += a[j];
         }
         System.out.println("сумма чисел до " + ( b - 1 ) +" = " + sum);
+        System.out.println("в массиве " + a.length + " значений, 999 из него = " + a[999]);
+
+        System.out.println();
+        System.out.println( "задачи с char");
+
+        char [] string = "КУ КУ МОЙ МАЛЬЧИК".toCharArray();
+        char [] string2 = new char[] {'H', 'E', 'L', 'L', 'O'};
+        char [] string3 = new char[3];
+        string3[0] = 'h';
+        string3[1] = 'e';
+        string3[2] = 'y';
+        for (int i = 0; i < string.length; i++) {
+            System.out.print(string[i]);
+        }
+        System.out.println();
+        for (int j = 0; j < string2.length; j++) {
+            System.out.println(string2[j]);
+        }
+        System.out.println();
+        for (int k = 0; k < string3.length; k++) {
+            System.out.print(string3[k]);
+
+        }
+
+
+
     }
 }
