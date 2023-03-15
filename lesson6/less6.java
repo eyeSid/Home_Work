dec6c9f6b77eed6a742677e35bd15b77f4286004
Sid&Du <sidorov.s.v@live.com>
2023-03-16 00:10:29 +0300

    урок 61

--- lesson6/less6.java
+++ lesson6/less6.java
@@ -14,6 +14,25 @@ public class less6 {
         }
         System.out.println("наименьшее число массива а = " + value);
         System.out.println("sum a = " + sum);
+                System.out.println("\n");
+//---------------------------------------------------------------------------------------
+//       если массив будет пустой то - int value = a[0]; - выдаст ошибку
+//       для этого используем - int value2 = Integer.MAX_VALUE; - присвоение
+//       значения переменной и Integer.MIN_VALUE - присвоение min значения переменной
+
+        int[] a2 = {10, 2 , 3, 4, 8, 9};
+
+        int value2 = Integer.MAX_VALUE; // - присвоение максимального значения переменной
+        for (int i = 0; i < a2.length; i++) {
+            if (value2 > a2[i]) {
+                value2 = a2[i];
+            }
+        }
+        System.out.println("наименьшее число массива а2 = " + value2);
+        System.out.println("\n");
+
+
+
 
         int[][] b = {{10, 2} , {3, 4, 1}, {}, {4}};
         int sum2 = 0;
@@ -28,18 +47,6 @@ public class less6 {
         }
         System.out.println("min b = " + min);
         System.out.println("сумма массива b = " + sum2);
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
