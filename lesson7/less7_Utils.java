16bab5e370a3fa6cc3e1ba2652b9c922cdef1b9e
Sid&Du <sidorov.s.v@live.com>
2023-03-20 00:10:48 +0300

    урок 61

---
+++ lesson7/less7_Utils.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+public class less7_Utils {
+    void printSum(int first, int second) {
+        int s = sum(first, second);
+        System.out.println("sum = " + s);
+    }
+    int sum(int firstSummand, int secondSummand) {
+        int sum = firstSummand + secondSummand;
+
+        return sum;
+    }
+
+    void sumOfThree(int ferstValue, int secondValue, int thirdValue) {
+        System.out.println("sum of 3 = " + (ferstValue + secondValue + thirdValue));
+    }
+    void div(int ky, int py) {
+        double div = 1.0 * ky / py;
+        System.out.println("div = " + div);
+    }
+}
