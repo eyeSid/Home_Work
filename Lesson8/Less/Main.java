7786449cf90148796051bede35eb86324819bd5a
Sidorov_S.V <126830059+eyeSid@users.noreply.github.com>
2023-03-23 17:05:04 +0300

    work

--- Lesson8/Less/Main.java
+++ Lesson8/Less/Main.java
@@ -1,5 +1,13 @@
 package Lesson8.Less;
 
+
+//  ВИДЫ объявления классов переменных и тд
+//public - виден всем
+//package - виден всем кто с ним в одной папке
+//protected - виден наследникам
+//private - не виден ни кому
+
+
 public class Main {
     public static void main(String[] args) {
         Worker worker = new Worker();
