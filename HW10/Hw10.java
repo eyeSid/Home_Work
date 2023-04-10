f993dd038ea8508db422f03552181c2a73f9e1d7
Sidorov_S.V <126830059+eyeSid@users.noreply.github.com>
2023-04-10 18:34:12 +0300

    H.work10

--- HW10/Hw10.java
+++ HW10/Hw10.java
@@ -1,24 +1,55 @@
 package HW10;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class Hw10 {
     public static String abbrevName(String name) {
-       // name = "dfg fgh";
-        return name;// = (n + ". " + f);
-
-    }
-    
-    public static void main(String[] args) {
-        String name = "bob djonson";
-
-        String[] c = abbrevName(name.toUpperCase()).split(" ");
+      //  name = name.toUpperCase();
+        String[] c = name.toUpperCase().split(" ");
         char n = c[0].charAt(0);
         char f = c[1].charAt(0);
-        name = (n + ". " + f);
-
-        System.out.println(abbrevName(name));
+        return name = (n + "." + f);
 
     }
+    public static boolean feast(String beast, String dish) {
+        boolean allowed = false;
+        char b = beast.charAt(0);
+        char d = dish.charAt(0);
+        char bb = beast.charAt(beast.length() - 1);
+        char dd = dish.charAt(dish.length() - 1);
+
+      if (b == d && bb == dd){
+          allowed = true;
+        } else {
+          allowed = false;
+      }
+        return allowed;
+    }
+
+    public static void main(String[] args) {
+        String name = "bobi Zjonson";
+        System.out.println(abbrevName(name));
+
+
+        String beast = "qiiir";
+        String dish = "qeeeer fg";
+        System.out.println(dish.charAt(dish.length() - 1));
+
+    }
+    public static String tripleTrouble(String one, String two, String three) {
+        String trouble = null;
+        String[] triple = {one, two, three};
+
+        for (int i = 0; i < triple.length; i++) {
+        char firstTriple = triple[0].charAt(i);
+        char secondTriple = triple[1].charAt(i);
+        char  thirdTriple = triple[2].charAt(i);
+        trouble = trouble + firstTriple + secondTriple + thirdTriple;
+        }
+        return trouble;
+    }
+
+
 
 }
